897414728061dda6b36abf17d655af42b7d9de8c
Ravinder Kumar <ravinderverma872@gmail.com>
2024-09-06 23:26:16 -0500

    Loading the model

--- House-Price-Prediction-using-LR/app.py
+++ House-Price-Prediction-using-LR/app.py
@@ -8,3 +8,13 @@ app=Flask(__name__)
 
 #Load the model
 reg_model=pickle.load(open('regmodel.pkl', 'rb'))
+
+@app.route('/')
+
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',method=['POST'])
+
+def predict_api():
+    data=request.json['data']
